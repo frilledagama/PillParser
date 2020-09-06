0345431c1a447dd2c88c81118128053302a7bc63
frilledagama <thatbiiih@gmail.com>
2020-09-06 03:22:21 -0400

    Add User Sign-In functinoality

---
+++ app/src/main/java/com/mh2020/android/pillparser/RegistrationActivity.java
@@ -0,0 +1,6 @@
+package com.mh2020.android.pillparser;
+
+// code from https://blog.usejournal.com/firebase-email-and-password-authentication-for-android-e335c81a1dad
+
+public class RegistrationActivity {
+}
