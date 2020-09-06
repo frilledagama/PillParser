6c4c4c7bf3b5b66e628c8ee0c18f93b9c1228451
frilledagama <thatbiiih@gmail.com>
2020-09-06 03:22:21 -0400

    Add User Sign-In functinoality

--- app/src/main/java/com/mh2020/android/pillparser/MainActivity.java
+++ app/src/main/java/com/mh2020/android/pillparser/MainActivity.java
@@ -1,10 +1,18 @@
 package com.mh2020.android.pillparser;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,8 +20,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+//import androidx.multidex.MultiDex;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -25,15 +35,28 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.Query;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    //private FirebaseAuth mAuth;
     private RecyclerView perscriptionList;
     private FirebaseFirestore firebaseFirestore;
     private FirestoreRecyclerAdapter adapter;
+    //recognize when the user comes back from the image gallery Activity
+    public static final int IMAGE_PICK_CODE = 1000;
+    public static final int PERMISSION_CODE = 1001;
+    public int morningNight = 1;
+    InputImage image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +65,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+        //Initialize FirebaseAuth mAuth = FirebaseAuth.getInstance();
         perscriptionList = findViewById(R.id.perscription_list);
         firebaseFirestore = FirebaseFirestore.getInstance();
 
@@ -67,8 +89,12 @@ public class MainActivity extends AppCompatActivity {
                 holder.perscription_name.setText(model.getMedicationName());
                 holder.dosage.setText(model.getDosage() + "mg");
                 holder.instructions.setText(model.getInstructions());
-                holder.image.setImageDrawable(getDrawable(R.drawable.ic_wb_sunny_black_24dp));
-
+                if (morningNight%2 == 0){
+                    holder.image.setImageDrawable(getDrawable(R.drawable.ic_brightness_3_black_48dp));
+                } else {
+                    holder.image.setImageDrawable(getDrawable(R.drawable.ic_wb_sunny_black_24dp));
+                }
+                    morningNight++;
             }
         };
 
@@ -81,19 +107,124 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent cameraIntent = new Intent(android.media.action.IMAGE_CAPTURE);
+                //get and image from the gallery
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED){
+                        ///permission not granted, request it
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    }
+                    else {
+                        //permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+
+
+                /*
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+                */
+
+                //Intent cameraIntent = new Intent(android.media.action.IMAGE_CAPTURE);
+                /*
+                InputImage image;
+                try {
+                    image = InputImage.fromFilePath(context, uri);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                 */
+
             }
         });
     }
+
+    private void pickImageFromGallery() {
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    //handle result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE: {
+                if (grantResults.length>0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission was granted
+                    pickImageFromGallery();
+                }
+                else{
+                    //permission was denied
+                    Toast.makeText(this, "Permission denied",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE && null != data) {
+            //send image to google?????
+            Uri selectedImage = data.getData();
+
+            try {
+                image = InputImage.fromFilePath(this, selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //2. Get an instance of TextRecognizer
+        TextRecognizer recognizer = TextRecognition.getClient();
+
+        //3. Process the image
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                // Task completed successfully
+                                // ...
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
         adapter.startListening();
     }
 
@@ -102,12 +233,12 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         adapter.stopListening();
     }
-
+/*
     private void updateUI(FirebaseUser currentUser) {
         //add things that will show up when a user logs in : )
         //reeeee
     }
-
+*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -129,8 +260,15 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
+    /*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+            //TODO: action
+        }
+    }
+    */
 
     private class PerscriptionViewHolder extends RecyclerView.ViewHolder{
         private ImageView image;
@@ -148,4 +286,12 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+/*
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+*/
 }
+
